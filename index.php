5fc17fa01392ca2a764285f2b45bb251d3dbd030
Bruno Gustavo Busarello <150341182+brunobusarello@users.noreply.github.com>
2024-06-20 16:52:15 -0300

    adicionado novo estilo ao arquivo principal

--- index.php
+++ index.php
@@ -3,21 +3,43 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Listagem de Jogos</title>
+    <title>Game Manager</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="shortcut icon" href="icones/favicon.ico" type="image/x-icon">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link rel="stylesheet" href="estilos/style.css">
-	<link rel="shortcut icon" href="icones/favicon.ico" type="image/x-icon">
 </head>
 <body>
-	<?php
+    <?php
 		// "COPIA" o arquivo banco.php
 		require_once "includes/banco.php";
 		require_once "includes/funcoes.php"
 	?>
-    <div id="corpo">
-        <h1>Escolha seu jogo</h1>
-        <div class="table">
-            <table class="listagem">
-				<?php
+    <main>
+        <header>
+            <div>
+                <h1>Game Manager</h1>
+            </div>
+            <div class="search">
+                <input type="text" name="buscar" id="buscar" placeholder="Buscar">
+                <label for="buscar">
+                    <span class="material-symbols-outlined">
+                        search
+                    </span>
+                </label>
+            </div>
+        </header>
+        <div class="table scroll">
+            <table>
+                <tr class="header">
+                    <td>ID</td>
+                    <td>Capa</td>
+                    <td>Jogo</td>
+                    <td>ADM</td>
+                </tr>
+                <?php
 					$busca = $banco->query("select * from jogos order by cod");
 					if(!$busca){
 						echo "<tr><td>Infelizmente a busca deu errado";
@@ -29,7 +51,8 @@
 						else{
 							while($reg = $busca->fetch_object()){
 								$t = thumb("$reg->capa");
-								echo "<tr><td><img src='$t' class='mini'>";
+                                echo "<tr><td>$reg->cod";
+								echo "<td><img src='$t' class='mini'>";
 								echo "<td><a href='detalhes.php?cod=$reg->cod'>$reg->nome</a>";
 								echo "<td>Adm";
 							}
@@ -38,9 +61,7 @@
 				?>
             </table>
         </div>
-		<?php
-			$banco->close();
-		?>
-    </div>
+        <footer></footer>
+    </main>
 </body>
 </html>
---
+++ indexv1.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listagem de Jogos</title>
+    <link rel="stylesheet" href="estilos/style.css">
+	<link rel="shortcut icon" href="icones/favicon.ico" type="image/x-icon">
+</head>
+<body>
+	<?php
+		// "COPIA" o arquivo banco.php
+		require_once "includes/banco.php";
+		require_once "includes/funcoes.php"
+	?>
+    <div id="corpo">
+        <h1>Escolha seu jogo</h1>
+        <div class="table">
+            <table class="listagem">
+				<?php
+					$busca = $banco->query("select * from jogos order by cod");
+					if(!$busca){
+						echo "<tr><td>Infelizmente a busca deu errado";
+					}
+					else{
+						if($busca->num_rows == 0){
+							echo "<tr><td>Nenhum registro encontrado";
+						}
+						else{
+							while($reg = $busca->fetch_object()){
+								$t = thumb("$reg->capa");
+								echo "<tr><td><img src='$t' class='mini'>";
+								echo "<td><a href='detalhes.php?cod=$reg->cod'>$reg->nome</a>";
+								echo "<td>Adm";
+							}
+						}
+					}
+				?>
+            </table>
+        </div>
+		<?php
+			$banco->close();
+		?>
+    </div>
+</body>
+</html>
